0589a06214771b27234a76d4544f30cbe54163f2
new <s6906021612010@email.kmutnb.ac.th>
2026-07-18 02:21:12 +0700

    new

--- Python/งานอ.สมชัย/บทที่ 4/Exercise4-2.py
+++ Python/งานอ.สมชัย/บทที่ 4/Exercise4-2.py
@@ -1,20 +1,20 @@
 print(">> Program Find Maximum Digit <<")
 
-while True:
-    value = int(input("Enter number of value(>=1) : "))
+value = int(input("Enter number of value(>=1) : "))
 
-    if value <= 0:
-        print("value input not correct.")
-        break
-
+if value <= 0:
+    print("value input not correct.")
+else:
+    num1=[]
     print(f"\nProgram get value {value} numbers.")
 
     for i in range(1, value + 1):
         num = int(input(f"Enter value Number #{i} : "))
-            
-        # if i == 1:
-            # print("Your enter number : ", num)
-            # print("Maximum number is : ", num)
-        
+        num1.append(num)
 
+print("Your enter number : ",",".join(str(n) for n in num1))
+
+max_num = max(num1)
+    
+print("Maximum value number is : ", max_num)
 print("Exit program")
