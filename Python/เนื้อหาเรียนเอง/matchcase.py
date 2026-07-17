feabd5d2ef54b9a3821180ad20cca492fedc7e1d
new <s6906021612010@email.kmutnb.ac.th>
2026-07-18 01:23:12 +0700

    Remove folders from repository

--- Python/เนื้อหาเรียนเอง/matchcase.py
+++
@@ -1,7 +0,0 @@
-#match-statement
-service= input("ป้อนหมายเลขบริการ (1-3): ")
-match service:
-    case "1":print("ถอนเงิน")
-    case "2":print("ฝากเงิน")
-    case "3":print("ยอดเงินคงเหลือในบัญชี")
-    case "":print("หมายเลขบริการไม่ถูกต้อง")
