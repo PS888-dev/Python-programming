5be29be79a148913046ada2b14cc24890d00ca64
PS888-dev <s6906021612010@email.kmutnb.ac.th>
2026-07-21 17:52:50 +0700

    new

---
+++ Python/พี่ติวน้อง/ex3.py
@@ -0,0 +1,34 @@
+#โปรแกรมตัดเกรด
+#input
+name = input("Enter You Name : ")
+score = int (input ("Enter You Score (0-100) : "))
+grade=""
+
+#pocess
+if score >= 80 and score <= 100: 
+    grade = "A"
+elif score >= 75 and score < 79: 
+    grade = "B+"
+elif score >=70 and score <=74: 
+    grade = "B"
+elif score >=65 and score <=69: 
+    grade = "c+"
+elif score >=60 and score <=64: 
+    grade = "c"
+elif score >=55 and score <=59: 
+    grade = "D+"
+elif score >=50 and score <=54: 
+    grade = "D"
+elif score >=45 and score <=49: 
+    grade = "F"
+else:
+    grade="N (Invalid)"
+
+#output
+print("="*25)
+print("Name : ", name)
+print("="*25)
+print("Score : ", score)
+print("="*25)
+print("Score  : You : Grade ", grade)
+print("="*25)
---
+++ Python/พี่ติวน้อง/new2.py
@@ -0,0 +1,18 @@
+# Age = int(input("Enter You Age : "))
+# if Age <= 12:
+#     print("เป็นเด็กดี")
+# elif Age <=18:
+#     print("เป็นวัยรุ่น")
+# else:
+#     print("เป็นผู้ใหญ่")
+
+
+choice = input("Enter Choice : ")
+if choice == "1":
+    print("Select 1")
+elif choice == "2":
+    print("Select 2")
+elif choice == "3":
+    print("Select 3")
+else:
+    print("Please select again (1-3.)")
