5c7ff129cdac9badb2f6a6a2e6fd2503266b9b9f
PS888-dev <s6906021612010@email.kmutnb.ac.th>
2026-07-21 17:52:58 +0700

    new

--- Python/พี่ติวน้อง/ex3.py
+++ Python/พี่ติวน้อง/ex3.py
@@ -5,24 +5,15 @@ score = int (input ("Enter You Score (0-100) : "))
 grade=""
 
 #pocess
-if score >= 80 and score <= 100: 
-    grade = "A"
-elif score >= 75 and score < 79: 
-    grade = "B+"
-elif score >=70 and score <=74: 
-    grade = "B"
-elif score >=65 and score <=69: 
-    grade = "c+"
-elif score >=60 and score <=64: 
-    grade = "c"
-elif score >=55 and score <=59: 
-    grade = "D+"
-elif score >=50 and score <=54: 
-    grade = "D"
-elif score >=45 and score <=49: 
-    grade = "F"
-else:
-    grade="N (Invalid)"
+if score >= 80 and score <= 100: grade = "A"
+elif score >= 75 and score < 79: grade = "B+"
+elif score >=70 and score <=74: grade = "B"
+elif score >=65 and score <=69: grade = "c+"
+elif score >=60 and score <=64: grade = "c"
+elif score >=55 and score <=59: grade = "D+"
+elif score >=50 and score <=54: grade = "D"
+elif score >=45 and score <=49: grade = "F"
+else:grade="N (Invalid)"
 
 #output
 print("="*25)
