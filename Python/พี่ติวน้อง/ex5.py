4b60632294849753368b107a4633b136e37af2e1
PS888-dev <s6906021612010@email.kmutnb.ac.th>
2026-07-21 19:12:47 +0700

    new

---
+++ Python/พี่ติวน้อง/ex5.py
@@ -0,0 +1,9 @@
+report = ""
+for i in range(5):
+    name = input("Enter Name : ")
+    report += name + " "
+print("NAME : ", report)
+
+total = 5
+for i in range(1, total + 1):
+    print("*" * i)
