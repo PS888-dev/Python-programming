ac3dfc6e3980408753187f3b6f621ec63430a56e
test <s69060216121010@email.kmutnb.ac.th>
2026-07-21 22:37:04 +0700

    new

--- Python/พี่ติวน้อง/ex5.py
+++ Python/พี่ติวน้อง/ex5.py
@@ -1,9 +1,9 @@
-report = ""
-for i in range(5):
-    name = input("Enter Name : ")
-    report += name + " "
-print("NAME : ", report)
+# report = ""
+# for i in range(5):
+#     name = input("Enter Name : ")
+#     report += name + " "
+# print("NAME : ", report)
 
 total = 5
 for i in range(1, total + 1):
-    print("*" * i)
+    print("*" * (total - i + 1))
