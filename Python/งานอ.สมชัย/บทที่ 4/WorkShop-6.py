8be501bc18cf1edd3d07c7356d311431de5a56f1
Tanawat120249 <tanawatbinuma.beer@gmail.com>
2026-07-24 10:14:52 +0700

    new

--- Python/งานอ.สมชัย/บทที่ 4/WorkShop-6.py
+++ Python/งานอ.สมชัย/บทที่ 4/WorkShop-6.py
@@ -2,6 +2,7 @@ total = 0.0
 Max = int(input("Enter max of score : "))
 for n in range(1, Max+1):
     score = float(input("Enter score #" + str(n) + " : "))
+    # score = float(input(f"Enter score #{n} : "))
     total = total + score
 print()
 print("Total score value : ", total)
