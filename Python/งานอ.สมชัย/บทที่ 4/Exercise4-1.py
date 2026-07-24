80f96c3f245d5ee01136a8e27fd6c7d6588bead5
PS888-dev <s6906021612010@email.kmutnb.ac.th>
2026-07-24 13:39:56 +0800

    new

--- Python/งานอ.สมชัย/บทที่ 4/Exercise4-1.py
+++ Python/งานอ.สมชัย/บทที่ 4/Exercise4-1.py
@@ -2,7 +2,14 @@ print(">>  Program Find Maximum Digit  <<")
 number=0
 while True:
     number=int(input("Enter integer number (0-exit) : "))
-    print(f"Maxumum Digit of intiger number {number} = {max(str(number))} ")
+
     if number<=0:
         break
+    
+    max_digit=0
+    for i in str(number):
+        if int(i) > max_digit:
+            max_digit=int(i)
+            
+    print(f"Maxumum Digit of intiger number {number} = {max_digit} ")
 print("Exit program")
