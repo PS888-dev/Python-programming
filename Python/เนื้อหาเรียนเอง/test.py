b939e469ca099f96e0e67b016243ee41c9614320
test <s69060216121010@email.kmutnb.ac.th>
2026-08-03 15:36:22 +0700

    new

---
+++ Python/เนื้อหาเรียนเอง/test.py
@@ -0,0 +1,12 @@
+name = (input("ชื่อพนักงาน :"))
+income = int(input("รายได้ :"))
+tax1,tax2 = 0.05,0.10
+print("*"*20)
+if income <= 500000:
+    tax1=income*tax1
+    print(f"เสียภาษี :{tax1}\nรายได้บริสุทธ์ :{income-tax1}")
+
+elif income >=500000:
+    tax1=income*tax2
+    print(f"เสียภาษี :{tax1}\nรายได้บริสุทธ์ :{income-tax1}")
+print("*"*20)
