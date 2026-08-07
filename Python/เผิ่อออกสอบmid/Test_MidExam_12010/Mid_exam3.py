084fa705bd19066da42856b7501521353589c597
new <s6906021612010@email.kmutnb.ac.th>
2026-08-08 00:59:23 +0700

    new

--- Python/เผิ่อออกสอบmid/Test_MidExam_12010/Mid_exam3.py
+++ Python/เผิ่อออกสอบmid/Test_MidExam_12010/Mid_exam3.py
@@ -1,28 +1,35 @@
 # # นายพศวีร์ จินดาประเสริฐ 6906021612010
-print(f"{" "*30}Report Grade")
-print(f"{"="*74}")
-print(f"| No. |{" "*6}Name Subject{" "*6} | Socre | Grade | Level | Credit | Point |")
 
-for i in range(1,6,+1):
-    subject = input(f"วิชา : ")
-    score = int(input(f"คะเเนน : "))
-    credit = int(input(f"หน่วยกิต : "))
+report = (f"{" "*30}Report Grade")
+bar = (f"{"="*76}")
+title = (f"| No. |{" "*7}Name Subject{" "*7} | Socre | Grade | Level | Credit | Point |")
+total_point = 0
+total_credit = 0
+total_all =""
+for i in range(5):
+    subject,score,credit = input("วิชา: "),int(input("คะเเนน : ")),int(input("หน่วยกิต : "))
+    
+    if score >= 80 and score <= 100 :grade, level = ("A"), 4.0
+    elif score >= 75 and score <= 79:grade, level = ("B+"), 3.5
+    elif score >= 70 and score <= 74:grade, level = ("B"), 3.0
+    elif score >= 65 and score <= 69:grade, level = ("C+"), 2.5
+    elif score >= 60 and score <= 64:grade, level = ("C"), 2.0
+    elif score >= 55 and score <= 59:grade, level = ("D+"), 1.5
+    elif score >= 50 and score <= 54:grade, level = ("D"), 1.0
+    else: grade, level = ("F"), 0.0
     i += 1
-    
-    if score >= 80 and score <= 100 :print("A")
-    elif score >= 75 and score <= 79:print("B+")
-    elif score >= 70 and score <= 74:print("B")
-    elif score >= 65 and score <= 69:print("C+")
-    elif score >= 60 and score <= 64:print("C")
-    elif score >= 55 and score <= 59:print("D+")
-    elif score >= 50 and score <= 54:print("D")
-    elif score >= 0 and score <= 49:print("F")
-    else:print("กรอกคะเเนนไม่ถูกต้อง")
-    
-# print(f"{" "*30}Report Grade")
-# print(f"{"="*74}")
-# print(f"| No. |{" "*6}Name Subject{" "*6} | Socre | Grade | Level | Credit | Point |")
+    total_point = level * credit
+    total_credit = total_point / credit
+    total_all += f"|  {i}  | {subject:<25} | {score:>3}   | {grade:>3}   | { level:^6}| {credit:^5}  | {total_point:^5.1f} |\n"
+total_all += f"{bar}\n| {"Total":^55} | {total_point:^7}|{total_credit:^7}|\n"
+total_all += f"{bar}\n|{"Grade Point Average (GPA) : ":>50} {total_point/total_credit:.2f}{"|":>20}\n{bar}"
 
-# print(f"{i}")
+print(f"{report}\n{bar}\n{title}\n{bar}")
+print(total_all)
+
+
+
+
+
 
 # print(len("| No. |       Name Subject        | Score | Grade | Level | Credit | Point |"))
